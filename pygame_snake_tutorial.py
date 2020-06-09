e2384b930e6d2695b2ded8a2b84a7ed9c1899c96
Faye Ly <fayedotly@gmail.com>
2020-06-09 14:31:08 -0700

    pysnake game tutorial on yt

---
+++ pygame_snake_tutorial.py
@@ -0,0 +1,16 @@
+# pygame snake youtube tutorial
+
+import turtle
+
+# screen
+wn = turtle.Screen()
+wn.title("Snake Game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0)
+
+# snake head
+
+
+
+wn.mainloop()
