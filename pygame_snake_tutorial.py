d3d2ebd5737561cc62ab931fcc982802133b9f9d
Faye Ly <fayedotly@gmail.com>
2020-06-09 22:13:35 -0700

    finised tutorial two

--- pygame_snake_tutorial.py
+++ pygame_snake_tutorial.py
@@ -1,16 +1,63 @@
 # pygame snake youtube tutorial
 
 import turtle
+import time
+
+delay = 0.1
 
 # screen
 wn = turtle.Screen()
 wn.title("Snake Game")
-wn.bgcolor("green")
+wn.bgcolor("black")
 wn.setup(width=600, height=600)
 wn.tracer(0)
 
 # snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("green")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
 
+# function
+def go_up():
+    head.direction = "up"
+def go_down():
+    head.direction = "down"
+def go_left():
+    head.direction = "left"
+def go_right():
+    head.direction = "right"
 
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# keyboard bindings
+wn.listen()
+wn.onkey(go_up, "w")
+wn.onkey(go_down, "s")
+wn.onkey(go_left, "a")
+wn.onkey(go_right, "d")
+
+# main game loop
+while True:
+    wn.update()
+
+    move()
+
+    time.sleep(delay)
 
 wn.mainloop()
